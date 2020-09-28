70bb73ff26925ebd3e5684d44ade68bc69fa58fa
moh syllah <lasy4u@hotmail.com>
2020-09-28 13:53:26 +0300

    Updates

---
+++ precache-manifest.cef2d5c0ab63b714e860463361444f36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f8622409384a39c9a483c4e28e87e5c",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "649915ff5922d3312802",
+    "url": "/myapp/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "bb32cafacf55a65d5d9e",
+    "url": "/myapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "649915ff5922d3312802",
+    "url": "/myapp/static/js/2.4785f13a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myapp/static/js/2.4785f13a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb32cafacf55a65d5d9e",
+    "url": "/myapp/static/js/main.05c30092.chunk.js"
+  },
+  {
+    "revision": "a10273ae4b5d6018bc69",
+    "url": "/myapp/static/js/runtime-main.434946cf.js"
+  }
+]);
